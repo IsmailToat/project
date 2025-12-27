9007c6b4dd0d5a0f63a1da0d29ec6843e499e66f
IsmailToat <1089701@hr.nl>
2025-12-27 16:03:05 +0100

    Fresh start

--- src/StarterPlayer/StarterCharacterScripts/InventoryLock.client.lua
+++
@@ -1,7 +0,0 @@
-local StarterGui = game:GetService("StarterGui")
-
--- Disable the Backpack (Hotbar) completely
--- This effectively locks the tool in your hand once equipped
-pcall(function()
-	StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
-end)
